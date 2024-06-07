e081e69f8ed0abb679a9c88e0b1f6c8b5ab63c4a
Lenovo T480 <anasrafiqanas@gmail.com>
2024-06-07 16:48:34 +0500

    first commit

---
+++ app/src/main/java/com/example/facebook/screen1.kt
@@ -0,0 +1,11 @@
+package com.example.facebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class screen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen1)
+    }
+}
